1379ef2d115ba0306020020882f64070149cd208
Annalise Alvarez <afalvarez0821@gmail.com>
2019-07-18 21:43:28 -0400

    challenge tier 1 and part of 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     public float speed;
     public float jumpForce;
+    public Text countText;
+    public Text winText;
+    public Text lifeText;
+    private int count;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        lifeText.text = "Lives: 3";
     }
 
     // Update is called once per frame
@@ -34,6 +44,23 @@ public class PlayerController : MonoBehaviour
             {
                 rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 4)
+        {
+            winText.text = "You Win!";
         }
     }
 }
